c4f7765492aee830c6b89d6cc0469590f57470e3
msb <msb20sobhi@gmail.com>
2024-08-08 13:55:58 +0300

    final

--- pages/2_About us.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.title("About Us")
-st.markdown("""**We are The coders team """)
-st.markdown("---")
---
+++ pages/3_About us.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("The coders")
+st.markdown("""🔳 Who we are?""")
+
+musaab_number = 966500283891
+
+
+st.markdown(f"- [Musab](https://www.linkedin.com/in/musabalsobhi/) - {musaab_number} ")
+st.markdown(f"- [Sarraa](https://www.linkedin.com/in/musabalsobhi/) - {musaab_number} ")
+st.markdown(f"- [Naif](https://www.linkedin.com/in/musabalsobhi/) - {musaab_number} ")
+st.markdown(f"- [Wadieah](https://www.linkedin.com/in/musabalsobhi/) - {musaab_number} ")
